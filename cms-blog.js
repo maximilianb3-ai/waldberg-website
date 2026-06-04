f562cc3e8cb6d7160f616dd37deb6eb1fe38fc4a
Maximilian Böttger <maximilianbottger@MacBook-Pro-von-Maximilian.local>
2026-06-04 21:51:46 +0200

    Add generated CMS data files

---
+++ cms-blog.js
@@ -0,0 +1,8 @@
+const CMS_BLOG = [
+  {
+    "date": "2026-06-04",
+    "title": "Test",
+    "excerpt": "test",
+    "slug": "2026-06-04-test"
+  }
+];
---
+++ cms-news.js
@@ -0,0 +1,8 @@
+const CMS_NEWS = [
+  {
+    "date": "2026-06-04",
+    "category": "info",
+    "title": "test",
+    "text": "test"
+  }
+];
