aec3e3244e08ab7e08a968500a2b39ae0be29ae9
Maximilian Böttger <maximilianbottger@MacBook-Pro-von-Maximilian.local>
2026-06-05 22:10:19 +0200

    Fix GitHub Action permissions + update CMS data

--- cms-blog.js
+++ cms-blog.js
@@ -4,17 +4,5 @@ const CMS_BLOG = [
     "title": "5 Vorteile des Wohnens in einer kleinen Genossenschaft – warum weniger",
     "excerpt": "Wer an Wohnungsgenossenschaften denkt, denkt oft an große Organisationen mit tausenden Wohnungen und anonymen Strukturen. Dabei sind es gerade die …",
     "slug": "2026-06-05-5-vorteile-des-wohnens-in-einer-kleinen-genossenschaft-–-warum-weniger-manchmal-mehr-ist"
-  },
-  {
-    "date": "2026-06-05",
-    "title": "5 Vorteile des Wohnens in einer kleinen Genossenschaft – warum weniger",
-    "excerpt": "Wer an Wohnungsgenossenschaften denkt, denkt oft an große Organisationen mit tausenden Wohnungen und anonymen Strukturen. Dabei sind es gerade die …",
-    "slug": "2026-06-05-5-vorteile-des-wohnens-in-einer-kleinen-genossenschaft-–-warum-weniger-manchmal-mehr-ist-1"
-  },
-  {
-    "date": "2026-06-04",
-    "title": "Test",
-    "excerpt": "test",
-    "slug": "2026-06-04-test"
   }
 ];
--- cms-news.js
+++ cms-news.js
@@ -1,5 +1,11 @@
 const CMS_NEWS = [
   {
+    "date": "2026-06-05",
+    "category": "info",
+    "title": "Neue Website",
+    "text": "Unsere neue Website ist seit Juli 2026 zu erreichen."
+  },
+  {
     "date": "2026-06-04",
     "category": "info",
     "title": "test",
