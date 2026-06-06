3b27662a12b50947f1505c7093fc696849a56bf2
Maximilian Böttger <maximilianboettger3@gmail.com>
2026-06-06 13:07:26 +0200

    Remove CMS, build pipeline and switch to static site

--- _news/2026-06-05-neues-dienstfahrzeug.md
+++
@@ -1,6 +0,0 @@
----
-title: "Neues Dienstfahrzeug "
-date: 2026-06-05T22:16:00.000+02:00
-category: info
----
-Es gibt ein neues Dienstfahrzeug.
--- cms-blog.js
+++
@@ -1,8 +0,0 @@
-const CMS_BLOG = [
-  {
-    "date": "2026-06-05",
-    "title": "5 Vorteile des Wohnens in einer kleinen Genossenschaft – warum weniger",
-    "excerpt": "Wer an Wohnungsgenossenschaften denkt, denkt oft an große Organisationen mit tausenden Wohnungen und anonymen Strukturen. Dabei sind es gerade die …",
-    "slug": "2026-06-05-5-vorteile-des-wohnens-in-einer-kleinen-genossenschaft-–-warum-weniger-manchmal-mehr-ist"
-  }
-];
